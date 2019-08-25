f2c1984c0dc5b6ce0561fc01450388dfab5ef640
1101844537 <1101844537@qq.com>
2019-08-25 20:35:08 +0800

    雪花落地修改

--- jump_2/Lua/scene.lua
+++ jump_2/Lua/scene.lua
@@ -1,6 +1,7 @@
 -->scene-2
-function init_snow(speed, num)
+function init_snow(speed, num, hit_spr_flag)
   if not speed then speed = 1 end
+  if not hit_spr_flag then hit_spr_flag = 2 end
   if not num then num = 128 end
   local snows = {}
   for i=1, num do
@@ -15,13 +16,20 @@ function init_snow(speed, num)
   end
   local timer = newtimer()
 
+  local function is_land(sp)
+    if fget(mget(sp.x/8, (sp.y+speed)/8)) == hit_spr_flag then
+      sp.y = flr((sp.y+speed)/8)*8 - 1
+      return true
+    end
+  end
+
   local function update()
     for s in all(snows) do
         if not s.landed then
             s.y += s.speed
         end
-        if s.y >= 100 and not s.landed then
-            s.y = 100
+        if is_land(s) and not s.landed then
+            -- s.y = 100
             s.landed = true
             timer.add_timeout('snow_melt'..s.n, 1, function()
                 s.landed = false
