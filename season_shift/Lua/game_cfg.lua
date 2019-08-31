48a7dcec6459b5da238ee2c2641de471c05b83d5
1101844537 <1101844537@qq.com>
2019-08-31 23:38:03 +0800

    配置调整

--- season_shift/Lua/game_cfg.lua
+++ season_shift/Lua/game_cfg.lua
@@ -2,8 +2,8 @@
 
 cfg_player_acceleration_fast = 0.2 -- 跑步加速度
 cfg_player_acceleration_low = 0.3 -- 跑步减速度
-cfg_jump_speed = 4 -- 跳跃速度
-cfg_climb_speed = 2 -- 爬墙速度
+cfg_jump_speed = 3.2 -- 跳跃速度
+cfg_climb_speed = 1.6 -- 爬墙速度
 cfg_gravity = 0.4 -- 重力(向下的加速度)
 cfg_player_max_v = 1.6 -- 最大速度
 cfg_mogu_jump = 4 -- 采蘑菇跳跃速度
