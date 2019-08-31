935936e341ecd57127c558f78d0d18b68ba6dab1
1101844537 <1101844537@qq.com>
2019-08-31 11:16:38 +0800

    配置文件修改

--- season_shift/Lua/game_cfg.lua
+++ season_shift/Lua/game_cfg.lua
@@ -7,3 +7,36 @@ cfg_climb_speed = 2 -- 爬墙速度
 cfg_gravity = 0.4 -- 重力(向下的加速度)
 cfg_player_max_v = 1.6 -- 最大速度
 cfg_mogu_jump = 3 -- 采蘑菇跳跃速度
+
+cfg_levels_autumn = {
+  1 = {
+    player_start_pos = { -- 角色起始在关卡中的位置 单位（格）
+      x = 0,
+      y = 3,
+    },
+    camera_pos = { -- 相机位置 单位（格）
+      x = 0,
+      y = 0,
+    },
+    level_type = "noaml", -- 配置地图类型（可做到不同地图使用不同的地形配置）
+    enemys = { -- 敌人配置
+      caterpillar ={},
+      bees = {},
+    },
+  },
+  2 = {
+    player_start_pos = { -- 角色起始在关卡中的位置 单位（格）
+      x = 0,
+      y = 3,
+    },
+    camera_pos = { -- 相机位置 单位（格）
+      x = 16,
+      y = 0,
+    },
+    level_type = "noaml", -- 配置地图类型（可做到不同地图使用不同的地形配置）
+    enemys = { -- 敌人配置
+      caterpillar ={},
+      bees = {},
+    },
+  }
+}
