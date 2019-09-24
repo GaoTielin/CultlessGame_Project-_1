908d8bb10c963655dae7a6ae87239c895cd2bc02
HarpSun <srcasdf@qq.com>
2019-09-24 20:45:33 +0800

    删除printt

--- season_shift/Lua/global.lua
+++ season_shift/Lua/global.lua
@@ -442,7 +442,6 @@ function game_over()
 end
 
 function change_level(level)
-    printh(level)
   if level > 7 and game_season == "summer" then return end
   if level == 12 and game_season == "winter" then
     init_cg("newhome.p8", 0, 224, 10, function()
